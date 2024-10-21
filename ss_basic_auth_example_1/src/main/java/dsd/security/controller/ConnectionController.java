db64998f89a4a805c850881df0e153875bf330f4
dsddevs <dsddevs@gmail.com>
2024-10-21 05:49:33 +0500

    added_ss_basic_auth_example_1

---
+++ ss_basic_auth_example_1/src/main/java/dsd/security/controller/ConnectionController.java
@@ -0,0 +1,19 @@
+package dsd.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConnectionController {
+
+    @GetMapping
+    public String connectServer(){
+        return "Server connected";
+    }
+
+    @GetMapping("/data")
+    public String getData(){
+        return "SUCCESS: Data accepted!";
+    }
+
+}
